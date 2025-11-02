e376f685c0cc9d34c17c0e51c3762da0c56c3351
anasali8 <anawlma11@gmail.com>
2025-11-02 20:36:23 +0200

    add primeng

--- src/app/components/auth-nav/auth-nav.component.ts
+++ src/app/components/auth-nav/auth-nav.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+import { MenubarModule } from 'primeng/menubar';
+import { BadgeModule } from 'primeng/badge';
+import { AvatarModule } from 'primeng/avatar';
+import { InputTextModule } from 'primeng/inputtext';
+import { CommonModule } from '@angular/common';
+import { RippleModule } from 'primeng/ripple';
+
 
 @Component({
   selector: 'app-auth-nav',
   standalone: true,
-  imports: [],
+  imports: [MenubarModule, BadgeModule, AvatarModule, InputTextModule,RippleModule, CommonModule],
   templateUrl: './auth-nav.component.html',
   styleUrl: './auth-nav.component.scss'
 })
-export class AuthNavComponent {
+export class AuthNavComponent implements OnInit {
+ items: MenuItem[] | undefined;
 
+    ngOnInit() {
+        this.items = [
+            {
+                label: 'Login',
+                icon: 'pi pi-sign-in'
+            },
+            {
+                label: 'Register',
+                icon: 'pi pi-user-plus'
+            }
+        ];
+    }
 }
