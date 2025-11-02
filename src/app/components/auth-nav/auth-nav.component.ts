d18d752a31095dc6cead9982d9935fb16f01bc7b
anasali8 <anawlma11@gmail.com>
2025-11-02 23:52:11 +0200

    finish Register UI

--- src/app/components/auth-nav/auth-nav.component.ts
+++ src/app/components/auth-nav/auth-nav.component.ts
@@ -1,12 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MenuItem } from 'primeng/api';
 import { MenubarModule } from 'primeng/menubar';
-import { BadgeModule } from 'primeng/badge';
-import { AvatarModule } from 'primeng/avatar';
-import { InputTextModule } from 'primeng/inputtext';
-import { CommonModule } from '@angular/common';
-import { RippleModule } from 'primeng/ripple';
-import { log } from 'node:console';
+
 import { RouterLink } from '@angular/router';
 
 
