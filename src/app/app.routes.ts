a3335b439d8fc706fcc7e190319d421a683dd2f4
anasali8 <anawlma11@gmail.com>
2025-11-02 19:36:27 +0200

    first-upload

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'auth', pathMatch: 'full' },
+  {
+    path: 'auth',
+    loadComponent: () =>
+      import('./layout/auth-layout/auth-layout.component').then(
+        (m) => m.AuthLayoutComponent
+      ),children:
+      [
+        {path:'', redirectTo:'login', pathMatch:'full'},
+        {path:'login', loadComponent:()=> import('./pages/login/login.component').then(l=> l.LoginComponent)},
+        {path:'register', loadComponent:()=> import('./pages/register/register.component').then(r=> r.RegisterComponent)}
+      ]
+  },
+  {
+    path: 'user',
+    loadComponent: () =>
+      import('./layout/user-layout/user-layout.component').then(
+        (m) => m.UserLayoutComponent
+      ),
+  },
+];
