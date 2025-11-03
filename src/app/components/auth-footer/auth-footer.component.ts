f396d9951dabd5db4b538d7af2ba9f96b5515ded
MoustafaDarweeshDev <ahmed_salama99@outlook.com>
2025-11-03 18:33:45 +0200

    footer and nav push

--- src/app/components/auth-footer/auth-footer.component.ts
+++ src/app/components/auth-footer/auth-footer.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DividerModule } from 'primeng/divider';
+import { ButtonModule } from 'primeng/button';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-auth-footer',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, DividerModule, ButtonModule, RouterLink],
   templateUrl: './auth-footer.component.html',
   styleUrl: './auth-footer.component.scss'
 })
 export class AuthFooterComponent {
-
+    currentYear: number = new Date().getFullYear();
 }
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
 
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { InputGroupModule } from 'primeng/inputgroup';
+import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule, InputGroupModule, InputGroupAddonModule, InputTextModule, ButtonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+
+  constructor() { }
+  formGroup: FormGroup = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6), Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{6,}$')]),
+  }
+);
+
+loginSubmit(){
+
+}
 
 }
