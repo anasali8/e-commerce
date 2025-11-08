4c725f297ced8efc21e8358892c9ce6b6a78440d
ahmed-salama07 <ahmed_salama99@outlook.com>
2025-11-08 19:32:50 +0200

    added carousel / products in home page

--- src/app/core/services/products/product-service.service.ts
+++ src/app/core/services/products/product-service.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { API_BASE_URL } from '../../apiRoot/baseUrl';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({
@@ -11,7 +10,7 @@ export class ProductServiceService {
   constructor(private httpClient: HttpClient) { }
 
   getProducts() : Observable<any>{ 
-    return this.httpClient.get(`${API_BASE_URL}/api/v1/products`);
+    return this.httpClient.get(`https://ecommerce.routemisr.com/api/v1/products`);
   }
 }
   
